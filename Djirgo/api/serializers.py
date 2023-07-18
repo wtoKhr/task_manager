57a25bc13bd40aea0d73c810d6e1a77892821fce
Сергей Христолюбов <skhrist@yandex.ru>
2023-07-18 12:36:08 +0300

    model/serializer fix

--- Djirgo/api/serializers.py
+++ Djirgo/api/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 
 from task.models import Task, User
-import datetime as dt
+import datetime
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    #age = serializers.SerializerMethodField()
+    age = serializers.SerializerMethodField()
     owner = serializers.PrimaryKeyRelatedField(read_only=True)
     
     class Meta:
@@ -14,8 +14,14 @@ class TaskSerializer(serializers.ModelSerializer):
         fields = '__all__'
         #read_only_fields = ('owner',)
 
-    # def get_age(self, obj):
-    #     return dt.datetime.now() - obj.pub_date
+    def get_age(self, obj):
+        age = obj.update_date - obj.pub_date
+        total_seconds = age.total_seconds()
+        hours = int(total_seconds // 3600)
+        minutes = int((total_seconds % 3600) // 60)
+        seconds = int(total_seconds % 60)
+        formatted_age = "{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds)
+        return formatted_age
 
 
 class UserSerializer(serializers.ModelSerializer):
